75c3a027de0cbfbf786fce6a8f756ae50d8b13ff
Prashanth <prashanthgadipelli@gmail.com>
2019-12-20 22:23:04 +0530

    JQuery API and API Call

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.static('public'))
-
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
+var obj={}
+obj.name = "Prashanth";
+obj.college = "CMRCET";
+obj.regno = "17H51A0518";
+
+app.get('/data', (req, res) => res.json(obj))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
